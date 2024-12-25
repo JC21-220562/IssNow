31cde5446171bc5c4386cbde98db82e49061c07a
涼太郎 武田 <220562@jc-21.jp>
2024-12-25 12:26:26 +0900

    修正

--- api/IssPosition.cs
+++ api/IssPosition.cs
@@ -50,7 +50,7 @@ namespace IssNow.api
             {
                 if (double.TryParse(Latitude, out double lat))
                 {
-                    return lat >= 0 ? "S" : "N";
+                    return lat >= 0 ? "N" : "S";
                 }
                 return "Invalid"; // エラー時の値
             }
